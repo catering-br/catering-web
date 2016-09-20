0b2ceebdf5e5098c8c69850a2a62ffe7692c3b6a
marielaDellInspiron <mariela@vision.ime.usp.br>
2016-09-20 00:22:45 -0300

    Starting the initial page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+  layout "application"
   protect_from_forgery with: :exception
 
   def start_page
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'welcome/index'
+
   resources :transactions
   resources :products
   resources :credit_cards
@@ -9,5 +11,6 @@ Rails.application.routes.draw do
   resources :telephones
   resources :clients
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root "application#start_page"
+  # root "application#start_page"
+  root "welcome#index"
 end
