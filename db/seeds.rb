8b412e21be8b04c03372a38bdedd8dc39da0cef8
Miguel Angel Galarreta V <miguelg@ime.usp.br>
2016-09-20 11:24:41 -0300

    adding seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,21 +6,26 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Telephone.delete_all
+
 
 
 Client.delete_all
-Client.create! login: "user1","u@h.com", "password",234
-Client.create! login: "user2","a@h.com", "pass",123
-Client.create! login: "usProf","profesional@hotmail.com", "pass",456
+c1 = Client.create! login: "user1", email:"u@h.com", name:"name User", password:"password",cpf:245
+c2 = Client.create! login: "user2",email:"a@h.com", name:"name User2", password:"pass",cpf:1239874562
+Client.create! login: "usProf", email:"profesional@hotmail.com", name:"name User 3", password:"pass", cpf:456987878
+
+Telephone.delete_all
+Telephone.create! client:c1, telephone: 23444543
+#Telephone.create! telephone: 65574839
 
 Professional.delete_all
-Professional.create!
+p1 = Professional.create! client: c1
+p2 = Professional.create! client: c2
 
 Product.delete_all
-Product.create! id: 1, name: "Banana", price: 0.49, active: true
-Product.create! id: 2, name: "Apple", price: 0.29, active: true
-Product.create! id: 3, name: "Carton of Strawberries", price: 1.99, active: true
+Product.create! professional: p1, name: "Banana", price: 0.49, description: "description", rating: 2
+Product.create! professional: p1, name: "Apple", price: 0.29, description: "description2", rating: 3
+Product.create! professional: p2, name: "Carton of Strawberries", price: 1.99, description: "description3", rating: 4
 
 
 
