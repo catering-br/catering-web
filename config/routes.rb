0536eea09b28ffa5c0b440e75280b29f5b866cd9
Gustavo <ghmzanon@gmail.com>
2016-09-21 23:03:38 -0300

    Client login, logout and registration --devise gem

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :clients
+  devise_for :clients, controllers: {registrations: 'clients/registrations'}
   resources :main_clients
   get 'welcome/index'
   # get 'welcome/index', to: 'main_clients#index', as: 'open_main_client'
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160919014836) do
+ActiveRecord::Schema.define(version: 20160922010222) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -39,12 +39,22 @@ ActiveRecord::Schema.define(version: 20160919014836) do
 
   create_table "clients", force: :cascade do |t|
     t.string   "login"
-    t.string   "email"
-    t.string   "password"
     t.string   "name"
     t.integer  "cpf"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.inet     "current_sign_in_ip"
+    t.inet     "last_sign_in_ip"
+    t.index ["email"], name: "index_clients_on_email", unique: true, using: :btree
+    t.index ["reset_password_token"], name: "index_clients_on_reset_password_token", unique: true, using: :btree
   end
 
   create_table "consumers", force: :cascade do |t|
