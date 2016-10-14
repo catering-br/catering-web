3169812994dfbf02a6e4fee6ee3029bb74b180b0
Mariela Atausinchi Fernandez <maratausinchi@gmail.com>
2016-10-14 00:08:21 -0300

    adding files of bootstrap

---
+++ app/assets/javascripts/welcome/manifest.js
@@ -0,0 +1,11 @@
+//= require jquery
+//= require jquery_ujs
+//= require bootstrap
+//= require welcome/cbpAnimatedHeader
+//= require welcome/classie
+//= require welcome/jquery.easing.min
+//= require welcome/jquery.fittext
+//= require welcome/wow.min
+//= require welcome/creative
+//= require_self
+
