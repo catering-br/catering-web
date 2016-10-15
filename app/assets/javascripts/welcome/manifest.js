16639ecc6183f72096371723e5e5294029c33a33
Mariela Atausinchi Fernandez <maratausinchi@gmail.com>
2016-10-15 00:52:34 -0300

    customizing sig-in and sig-up views. entrar/cadastrar buttons added in welcome page. Partial customizing of products views (products filter is not working)

--- app/assets/javascripts/welcome/manifest.js
+++ app/assets/javascripts/welcome/manifest.js
@@ -1,6 +1,7 @@
 //= require jquery
 //= require jquery_ujs
 //= require bootstrap
+//= require bootstrap.min
 //= require welcome/cbpAnimatedHeader
 //= require welcome/classie
 //= require welcome/jquery.easing.min
